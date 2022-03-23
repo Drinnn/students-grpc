85e2b613b5793f1be23c20d774368ec9059a5e54
Drinnn <pedro_lontro@hotmail.com>
2022-03-23 11:51:41 -0300

    feat: add bi-directional stream on client

--- cmd/client/grpc.go
+++ cmd/client/grpc.go
@@ -22,7 +22,8 @@ func main() {
 	
 	// AddStudent(client)
 	// AddStudentVerbose(client)
-	AddStudents(client)
+	// AddStudents(client)
+	AddStudentStreamBoth(client)
 }
 
 func AddStudent(client protos.StudentServiceClient) {
@@ -110,4 +111,66 @@ func AddStudents(client protos.StudentServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddStudentStreamBoth(client protos.StudentServiceClient) {
+	stream, err := client.AddStudentStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*protos.Student{
+		{
+			Id: "1",
+			Name: "John",
+			Email: "john@mail.com",
+		},
+		{
+			Id: "2",
+			Name: "Adam",
+			Email: "adam@mail.com",
+		},
+		{
+			Id: "3",
+			Name: "Doe",
+			Email: "doe@mail.com",
+		},
+		{
+			Id: "4",
+			Name: "Abdhu",
+			Email: "abdhu@mail.com",
+		},
+		{
+			Id: "5",
+			Name: "Wesley",
+			Email: "wesley@mail.com",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+			}
+			fmt.Printf("Receiving student %v with status: %v\n", res.Student.Name, res.Status)
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
